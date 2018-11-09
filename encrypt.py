801942bc40de8d47fa7dfbd818cd0cbe90532a70
quangle197 <37059094+quangle197@users.noreply.github.com>
2018-11-08 17:09:04 -0800

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -11,22 +11,28 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import asymmetric
 
-
+keySize = 32
+ivSize = 16
+blockSize = 128
 
 #Encrypt function
 def MyEncrypt(m,key):
-    if(len(key)<32):
+    if(len(key)<keySize):
         print("Key is < 32 byte")
         return
     
     
     backend = default_backend() #use default backend
-    iv = os.urandom(16) #generate 16 byte iv
+    iv = os.urandom(ivSize) #generate 16 byte iv
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend) #cipher in AES and CBC mode
     encryptor = cipher.encryptor() # make an encryptor
     m=base64.b64encode(m)#encode the message using base64 to encrypt
-    padder = padding.PKCS7(128).padder()#create a padder
+    padder = padding.PKCS7(blockSize).padder()#create a padder
     paddata = padder.update(m) # pad the message by appending N-1 bytes with the value of 0 and a last byte with the value of chr(N)
     paddata += padder.finalize() #Finalize the current context and return the rest of the data.
     
@@ -40,7 +46,7 @@ def MyDecrypt(m,key,iv):
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
     decryptor = cipher.decryptor()# make a decryptor
     m=decryptor.update(m) + decryptor.finalize() #decrypt the m
-    unpad = padding.PKCS7(128).unpadder()#make an unpadder
+    unpad = padding.PKCS7(blockSize).unpadder()#make an unpadder
     data = unpad.update(m) #unpad the decrypted data
     data += unpad.finalize() #Finalize the current context and return the rest of the data.
     m=base64.b64decode(data) #decode the data using base64
@@ -48,7 +54,7 @@ def MyDecrypt(m,key,iv):
 
 #Encrypt a file function
 def MyfileEncrypt(filepath):#filepath to the file
-    key = os.urandom(32) #make a 32 byte key
+    key = os.urandom(keySize) #make a 32 byte key
 
     file = open(filepath,"rb")#open the file to encrypt
     m =file.read()# read the file
@@ -71,7 +77,6 @@ def MyfileDecrypt(filepath,key,iv):
 
 def MyencryptMAC(message, EncKey, HMACKey):
     m,iv= MyEncrypt(message,EncKey) #get the m and iv from encrypt
-    
     h= hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend()) #make an hmac
     h.update(m) #create the hash 
     return (m,iv,h.finalize()) #return encrypted message and iv
@@ -79,10 +84,10 @@ def MyencryptMAC(message, EncKey, HMACKey):
 
 def MydecryptMAC(message,EncKey,HMACKey,iv,tag):
     try:
-        h = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend())
-        h.update(message)
-        h.verify(tag)
-        m=MyDecrypt(message,EncKey,iv)
+        h = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend()) #make an hmac
+        h.update(message) #recalculate the message
+        h.verify(tag) #compare to the tag
+        m=MyDecrypt(message,EncKey,iv) #Decrypt the message
         return m
     except:
         print("Invalid tag")
@@ -97,8 +102,8 @@ def MydecryptMAC(message,EncKey,HMACKey,iv,tag):
 # =============================================================================
     
 def MyfileEncryptMAC(filepath):
-    key = os.urandom(32) #make a 32 byte key
-    Hkey = os.urandom(32) #make a 32 byte hmac key
+    key = os.urandom(keySize) #make a 32 byte key
+    Hkey = os.urandom(keySize) #make a 32 byte hmac key
     
     file = open(filepath,"rb")#open the file to encrypt
     m =file.read()# read the file
@@ -113,19 +118,82 @@ def MyfileEncryptMAC(filepath):
 def MyfileDecryptMAC(filepath,EncKey,HMACKey,iv,tag):
     file = open(filepath,"rb") #open a file to decrypt
     content=file.read() #read the file
-    m=MydecryptMAC(content,EncKey,HMACKey,iv,tag)
+    m=MydecryptMAC(content,EncKey,HMACKey,iv,tag) #decrypt the file
     out_file1 = open(filepath, "wb") #make a new file
     out_file1.write(m) #write a new file
     out_file1.close() #close that file
-    
-m=b"test"
-key = os.urandom(32)
-hkey= os.urandom(32)
-message,IV,tag =MyencryptMAC(m,key,hkey)
-print(message)
-message = MydecryptMAC(message,key,hkey,IV,tag)
-print(message)
 
-C,IV,tag,Enckey,HMACKey,ext=MyfileEncryptMAC("test.jpg")
-inp=input("Press enter to continue")
-MyfileDecryptMAC("test.jpg",Enckey,HMACKey,IV,tag)
+pubKey= 'PublicKey.pem'
+priKey= 'PrivateKey.pem'
+
+def RSAKey(pubKey,priKey):
+    if(os.path.exists(pubKey) and os.path.exists(pubKey)):
+        print("Key files exist")
+    else:
+        private_key = rsa.generate_private_key(public_exponent=65537,\
+                                               key_size=2048,\
+                                               backend=default_backend())
+        
+        public_key = private_key.public_key()  
+    
+        pub = public_key.public_bytes(encoding=serialization.Encoding.PEM,\
+                                      format=serialization.PublicFormat.SubjectPublicKeyInfo )
+        #pub.splitlines()[0]
+        
+        pri =private_key.private_bytes(encoding=serialization.Encoding.PEM,\
+                                       format=serialization.PrivateFormat.TraditionalOpenSSL,\
+                                       encryption_algorithm=serialization.NoEncryption())
+        #pri.splitlines()[0]
+        
+        pubFile=open(pubKey,"wb")
+        pubFile.write(pub)
+        pubFile.close()
+        
+        
+        priFile=open(priKey,"wb")
+        priFile.write(pri)
+        priFile.close()
+        
+    
+def  MyRSAEncrypt(filepath, RSA_Publickey_filepath):
+     (C, IV, tag, Enckey, HMACKey, ext)= MyfileEncryptMAC(filepath)
+     key = Enckey + HMACKey
+     with open(pubKey, "rb") as key_file:
+         pub = serialization.load_pem_public_key(key_file.read(),\
+                                                          backend=default_backend())
+     RSACipher = pub.encrypt(key,\
+                                   asymmetric.padding.OAEP(\
+                                                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),\
+                                                algorithm=hashes.SHA256(),\
+                                                label=None))
+     
+     return RSACipher,C, IV, tag, ext
+    
+
+def MyRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_filepath):
+    with open(priKey, "rb") as key_file:
+         private = serialization.load_pem_private_key(key_file.read(),\
+                                                          password=None,\
+                                                          backend=default_backend())
+    key= private.decrypt(RSACipher,\
+                             asymmetric.padding.OAEP(mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),\
+                                          algorithm=hashes.SHA256(),\
+                                          label=None))
+    EncKey=key[:32]
+    HMACKey= key[-32:]
+    
+    m=MydecryptMAC(C,EncKey,HMACKey,IV,tag)
+    out_file1 = open("Decyptedfile", "wb") #make a new file
+    out_file1.write(m) #write a new file
+    out_file1.close() #close that file
+    
+    
+    
+
+
+#C,IV,tag,Enckey,HMACKey,ext=MyfileEncryptMAC("test.jpg")
+#inp=input("Press enter to continue")
+#RSAKey(pubKey,priKey)
+
+RSACipher,C, IV, tag, ext= MyRSAEncrypt("test.jpg",pubKey)
+MyRSADecrypt(RSACipher, C, IV, tag, ext, priKey)
